8455ffc87894b5c746f4693b542cd46f7081eae5
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-22 03:30:44 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_character.rb
+++ db/migrate/006_add_catchphrase_to_character.rb
@@ -1,4 +1,4 @@
-class AddCatchphraseToCharacter < AcitveRecord::Migration[5.1]
+class AddCatchphraseToCharacter < ActiveRecord::Migration[5.1]
   def change
     add_column :characters, :catchphrase, :string
   end
