7ce5bc7da41e5cf438d63712e004cb69679613e6
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-22 04:26:51 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,9 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+
+  def actors_list
+    characters.collect do |char|
+      char.actor.full_name
+  end
 end
